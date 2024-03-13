c5811d548baa62fe6662f558d49472e84705bcd1
Lessalc <lucianoclessa@gmail.com>
2024-03-12 23:40:43 -0300

    Inicializando o projeto

---
+++ src/main/java/com/lessalc/apidevopstest/ApidevopstestApplication.java
@@ -0,0 +1,13 @@
+package com.lessalc.apidevopstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApidevopstestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApidevopstestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lessalc/apidevopstest/ApidevopstestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lessalc.apidevopstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApidevopstestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
